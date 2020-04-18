ed8f48fd9a69e13e0f80d8c5c66591434ec0719d
Josue Mercadillo <josuemercally@gmail.com>
2020-04-18 12:08:20 -0600

    Client room update.

--- WebApp/ClientApp/src/app/subscribe/subscribe.component.ts
+++ WebApp/ClientApp/src/app/subscribe/subscribe.component.ts
@@ -16,12 +16,28 @@ export class SubscribeComponent {
   }
 
   onClickSubscribe() {
+    // Creando conexión
     this.socket = io(this.socketUrl);
 
-    this.socket.on('message', (args) => {
-      console.log('message', args);
+    // Cambiando de room
+    this.socket.emit('switchRoom', 'room2');
+
+    // Evento cuando cambia de sala
+    this.socket.on('updateRooms', (rooms, current_room) => {
+      console.log('updateRooms', rooms, current_room);
     });
 
+    // Evento cuando actualiza estado cualquier socket en la sala
+    this.socket.on('updateRoom', (from, message) => {
+      console.log('updateRoom', from, message);
+    });
+
+    // Evento recibe mensaje generico
+    this.socket.on('updateServer', (args) => {
+      console.log('updateServer', args);
+    });
+
+    // Evento recibe mensaje especifico
     this.socket.on('new-user-queue', (args) => {
       console.log('new-user-queue', args);
     });

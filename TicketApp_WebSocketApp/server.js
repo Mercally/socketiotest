0c510401eed5a1a84b3880b8ffb90519cc0eb7cc
Josue Mercadillo <josuemercally@gmail.com>
2020-04-17 22:02:00 -0600

    Agregando logica de base de datos.

--- TicketApp_WebSocketApp/server.js
+++ TicketApp_WebSocketApp/server.js
@@ -1,21 +1,41 @@
 'use strict';
 
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const http = require('http');
 const app = express();
+
+const pg = require('pg');
+const pool = new pg.Pool({
+    host: 'database-test.cnbndhjcuspi.us-east-2.rds.amazonaws.com',
+    port: 5432,
+    database: 'test',
+    user: 'postgres',
+    password: '4dm1n3214dm1n321'
+});
 
 // Settings
 app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'public'))); // Static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 const server = app.listen(app.get('port'), () => {
     console.log('Server on port', app.get('port'));
+
+    // Postgres connection
+    pool.connect()
+        .then(client => {
+            client.query('LISTEN new_testevent');
+            client.on('notification', (data) => {
+                var payload = JSON.parse(data.payload);
+                console.log('row added', payload);
+                io.sockets.emit('new-user-queue', payload);
+            });
+            console.log('postgres connected successfully!');
+        }).catch(error => {
+            console.log('ERROR: postgres:', error);
+        });
 });
 
 const io = require('socket.io')(server);
-//io.listen;
 
 io.on('connection', (socket) => {
     console.log('a user connected');
